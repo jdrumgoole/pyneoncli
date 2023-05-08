f97abad0a6fa8012e51b89e6fe9509a0198608dc
Joe Drumgoole <joe@joedrumgoole.com>
2023-05-08 10:03:30 +0100

    Added pretty printing for JSON and refactored the validate command

--- src/pyneoncli_main.py
+++ src/pyneoncli_main.py
@@ -2,12 +2,34 @@ import argparse
 import os
 from neon_api import NeonAPI
 import pprint
+from typing import Any
+import json
+
+from pygments import highlight
+from pygments.formatters import TerminalTrueColorFormatter
+from pygments.lexers import JsonLexer
+
 NEON_API_KEY = None
-PGHOST= 5432
-PGHOST="localhost"
-PGUSER=os.getenv("USERNAME", "postgres")
-PGPASSWORD=""
-PGDATABASE="postgres"
+
+def pprint_color(obj: Any) -> None:
+    """Pretty-print in color."""
+    json_str = json.dumps(obj, indent=4, sort_keys=True)
+    print(highlight(json_str, JsonLexer(), TerminalTrueColorFormatter()))
+class CLI_Commands:
+
+    def __init__(self, api:NeonAPI) -> None:
+        self._api = api
+
+    def validate(self):
+        if self._api.validate_key():
+            print("key is valid")
+        else:
+            print("key is not valid")
+
+    def list(self):
+        for p in self._api.get_projects():
+            print(f"name: {p['name']}")
+            pprint_color(p)
 
 def get_default_from_env(env_var, default):
     return os.getenv(env_var, default
@@ -25,15 +47,14 @@ def main(apikey, pghost, pgport, pgurl, command=None):
     print(f"PostgreSQL Port: {pgport}")
     print(f"PostgreSQL URL: {pgurl}")
     api = NeonAPI(key=apikey)
-    if api.validate_key():
-        print("key is valid")
+    cmds = CLI_Commands(api)
+    
     if command:
         print(f"Command: {command}")
         if "list" in command:
-            for p in api.get_projects():
-                print(f"name: {p['name']}")
-                pprint.pprint(p)
-
+            cmds.list()
+        if "validate" in command:
+            cmds.validate()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process some arguments.')
@@ -41,7 +62,7 @@ if __name__ == '__main__':
     parser.add_argument('--pghost', type=str, help='PostgreSQL Host', default=os.getenv( "PGHOST", "localhost"))
     parser.add_argument('--pgport', type=str, help='PostgreSQL Port', default=os.getenv( "PGPORT", "5432"))
     parser.add_argument('--pgurl', type=str, help='PostgreSQL URL', default=os.getenv( "PGURL", "postgresql://localhost:5432"))
-    parser.add_argument("cmd", nargs="*", choices=["list"],  help="List projects")
+    parser.add_argument("cmd", nargs="*", choices=["validate", "list"],  help="List projects")
     args = parser.parse_args()
 
     main(args.apikey, args.pghost, args.pgport, args.pgurl, args.cmd)
